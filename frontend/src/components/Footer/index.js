6f56734afad1d48ef6958cfdb85af10f77b15745
Bilal Ahmad <BilalSheikh2500.com>
2022-01-03 12:31:45 +0500

    upload files

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="w-100">
+      <p
+        style={{ background: "#080808", fontSize: "20px", textAlign: "center" }}
+        className="text-white py-4 m-0"
+      >
+        made by Seerat Salman
+      </p>
+    </div>
+  );
+}
+
